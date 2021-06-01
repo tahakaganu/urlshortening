16df26006ad4a370cc33dd4e8e746a736723a98b
Daniel Santos <danielsantos@pobox.com>
2021-06-01 11:17:46 +0100

    Added docker composer

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,13 +1,18 @@
-from flask import Flask, jsonify
+import os
+from flask import Flask
 from werkzeug.utils import redirect
 from app.api.v1 import v1
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure, OperationFailure
+from flask_pymongo import PyMongo
 
 App = Flask(__name__)
 
 App.register_blueprint(v1)
 
+App.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
+mongo = PyMongo(App)
+db = mongo.db
 
 @App.route('/')
 def home():
@@ -18,4 +23,6 @@ def home():
     return redirect('v1/docs', code = 302)
 
 if __name__ == '__main__':
-    App.run()
+    ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
+    ENVIRONMENT_PORT = os.environ.get("APP_PORT", 8080)
+    App.run(host='0.0.0.0', port=ENVIRONMENT_PORT, debug=ENVIRONMENT_DEBUG)
